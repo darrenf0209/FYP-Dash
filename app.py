c175e19a866991e4f47b3edd89a7f8c6d8ac98b3
Darren Flaks <darrenf0209@gmail.com>
2020-07-29 09:01:01 +1000

    Adding in a temporary graph with dropdown menu

--- app.py
+++ app.py
@@ -1,15 +1,46 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+
+alt1_data = pd.read_csv("data\\alternative_1_20200527.csv")
+alt1_graph = go.Scatter(x=alt1_data['Iteration'],
+                        y=alt1_data['PSNR'],
+                        name="Alt1")
+
+data = [alt1_graph]
+layout = dict(title="Alternative 1", showlegend=False)
+fig = dict(data=data, layout=layout)
+# print(alt1_data['Loss'])
 
 # Initialising the app
 app = dash.Dash()
 
 # Container of div and html elements
-app.layout = html.Div(
-    html.H1(children='Supervised Causal Video Super-Resolution')
+app.layout = html.Div([
+    html.Div(
+        html.H1(children='Supervised Causal Video Super-Resolution')
+    ),
 
-)
+    html.Label("Model Training Results"),
+
+    html.Div(
+        dcc.Dropdown(
+            options=[
+                {'label': 'Control', 'value': 'Control'},
+                {'label': 'Null', 'value': 'Null'},
+                {'label': 'Alternative', 'value': 'Alt'}
+            ]
+        )
+    ),
+
+    html.Div(
+        dcc.Graph(id="Alt 1 Training",
+                  figure=fig)
+    )
+
+])
 
 if __name__ == "__main__":
     app.run_server(debug=True)
